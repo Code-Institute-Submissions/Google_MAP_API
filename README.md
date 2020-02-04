532d4437aef9a93f517461e523fab9901080dd60
petergalvin-cyber <petergalvin@consultant.com>
2020-02-04 16:04:12 +0000

    Mentor Feedback : Remove right click, improve UI

--- README.md
+++ README.md
@@ -1,7 +1,108 @@
-# Code Institute
+# Milestone Project 2 : Interactive FrontEnd
 
-Welcome petergalvin-cyber,
+## Project : Google MAPS and Google Places API interface.
+This project designs a weekend travel application for the UK domestic market that promotes travelling to London for Brexit protests.
+The design is focussed on London City only but has the functionality to include other UK cities. The site obtains revenue from sponsors that
+advertise various London travel packages and the users are encouraged to use these sponsors with offers of special deals on use of the site sponsor code.
 
-We have preinstalled all of the tools you need to get started.
+## UX
 
-Happy coding!
+### User Stories
+The site is aimed at users that travel to London to join Brexit protests and therefore has to be easy to use and offer very few options to 
+avoid confusion. The map is restricted to central london and the user can search for Hotels, pubs, food and entertainment by selecting options 
+or dragging the map. Location information is easy to digest and avoid excessive typing and text explanation. The options offered should appeal mostly to
+male users but options should consider married couples. As its a protest site the options should not cater for families.  
+
+The site appeals to the users nationalistic sentiment by using Union Jack flag and associated colours throughout. The user options are written in 
+upfront and direct style to appeal to the loud and aggressive nature of the protest.
+
+The site aims to exploit patriotic fervour aroused by Brexit and substantial product discounts to emotionally connect to users and encourage them to buy
+sponsored product.
+
+
+## Strategy
+Use nationisitc sentiment surrounding Brexit protests to promote a simple and easy to use London travel application and obtain revenue through 
+users purchasing from discounted sponsored sites. The site uses overt UK nationalism in the design and sets itself apart from mainstream coporate
+sites that wish to be neutral. Our site is focussed on London since its the centre of Brexit protests which is the site's foundation strategy.
+
+## Scope
+The map is restricted to central London and activity options offered are limited to Hotels, Bars, Restaurants and Museums (couples and sightseeing). 
+Since the Brexit protests visits are only short stay there is no requirement for an extensive range of activities.
+
+## Structure
+To promote ease of use the site will consist of one page. The header banner will declare nationalistic affiliation and the sponsors will be displayed 
+prominently to encourage use. The map is the centrepiece and will take up most of the page with specific location information displayed in the 
+footer to avoid cluttering up the map.
+
+## Skeleton
+The user selects activities or drags the map to generate markers which avoids text entry. Location information is displayed in the footer on clicking 
+the marker. The UK flag colours should feature in the site to be consistent with the patriotic theme. The site should be responsive to all devices.
+Information display is minimised to avoid overloading the user.
+
+WIREFRAMES
+
+## Surface
+UK flag and national colours used througout the site. Use colloquial language to engage the user and develop rapport. Sponsors displayed beside
+the map to encourage selection. The site only displays chosen activites to avoid crowding up the map.
+
+
+## Technologies
+1. HTML
+2. CSS.
+3. Javascript and JQuery.
+4. Google MAP, Geocoder and Places API.
+
+
+## Testing
+
+1. Javascript Google API
+Javascript API code was mainly based on Google Javascript API sample code. These functions were tested by comparing the results to online Google maps. 
+The main issue was differences in UK/US spelling and activity naming convention. The standard red marker ICONs were replaced by activity specific 
+ICONS based on the location 'types' and these were tested by comparing the sites retrieved on the Javascript search to online Google maps.
+
+1. Activities are revealed by selecting the radial buttons which activates the marker placement or dragging the map. These markers are cleared once 
+another option is selected or the map is once again dragged. This process was tested and operates effectively. The map also successfully restricts 
+selection to London city.
+
+2. DOM Interaction
+This was tested by clicking on multiple marker ICONs of various types to reveal place photo, address and rating in the footer. The existing DOM information
+is deleted once another marker is selected. The photo is not displayed on mobile devices to conserve space.
+
+3. The activity selector uses check boxes to ensure only one activity is selected and avoid crowding the map. This DOM input successfully feeds into 
+the Google maps API and various activities were tested.
+
+4. The screen design uses CSS grid functionality which is responsive to all devices. This enables the sponsor bar to be repositioned to a sidebar 
+for large screens. This framework is compatible with Chrome, Safari, Firefox and Edge but not with IE 10 or less.
+
+
+## Deployment
+
+
+## Credits
+All photo images were obtained from Google maps or Google images. Much of the Javascript code is based on the Google samples with site specific
+amendments. Stackover flow content was used to resolve callback issues and to use JQuery to extract and to push data to the DOM.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/js/map.js
+++ assets/js/map.js
@@ -11,14 +11,13 @@
 
     function initMap() {
         
+                                         // Find the start location city and display on the DOM
         start_centre = {lat: 51.506164642, lng: -0.124832834};
         Start_location(start_centre,function(start_city){
             $("span.start").text(start_city);
         });
         
-       
-        //document.getElementById('start').textContent = start_city; 
-
+                                        // New map element but with restrictions to prevent user going outside London.  
         map = new google.maps.Map(document.getElementById('map'),
            {
             center: start_centre,
@@ -44,48 +43,85 @@
 
          placesList = document.getElementById('text-display');
          img_parent = document.getElementById('img-display');
-         
-       
-        
-       map.addListener('rightclick', function(e){
-            
-         for (var i = 0; i < markers.length; i++) {  
-          markers[i].setMap(null); 
-         }     
-           markers = [];
+          
+                                      
+                                            // Obtain IDs of input list
 
-           var request = {
-              location: e.latLng,
-              radius: 1000,
-              types: [Select_Activity()]
-               };
+       var hotel = document.getElementById('Hotel');
+       var bar = document.getElementById('bar');
+       var eat = document.getElementById('restaurant');
+       var museum = document.getElementById('museum');
+       var clear = document.getElementById('clear');
 
+                                          // Add listener to input list, if any change then activate function
+
+       google.maps.event.addDomListener(hotel,'change',function() {
+           Search();
+       }); 
+
+       google.maps.event.addDomListener(bar,'change',function() {
+           Search();
+       }); 
+
+       google.maps.event.addDomListener(eat,'change',function() {
+           Search();
+       }); 
+
+       google.maps.event.addDomListener(museum,'change',function() {
+           Search();
+       }); 
+
+       google.maps.event.addDomListener(clear,'change',function() {
+           clearMarkers(markers);
+       }); 
+
+                                                // Once user stops dragging map then a search is commenced.
+       map.addListener('dragend', function(){
+            Search();       
+       });
+    }      // End InitMaP
+    
+
+
+    function Search () {
            
+           clearMarkers(markers);
+
+                            // Search current map bounds by selected activity in the DOM.
+           var request = {
+              bounds: map.getBounds(),
+              types: [Select_Activity()]
+            };
+          
            service.nearbySearch(request, function(results, status) {
              if (status == google.maps.places.PlacesServiceStatus.OK) {
-               for (var i = 0; i < results.length; i++) {             
-                 createMarker(results[i],i,request.types);
-                }
+               for (var i = 0; i < results.length; i++) {  
+                   createMarker(results[i],i,request.types);
+                  }
              }
             });
-  
-       });
-    }      
-                 
-    function createMarker(plot,index,place_type) {
-        var photoUrl;
 
-            markers[index] = new google.maps.Marker({
-                       map: map,
-                       draggable: false,
-                       animation: google.maps.Animation.DROP,
-                       position: plot.geometry.location,
-                       title: plot.name,
-                       icon: Set_Icon (place_type) 
-                     });
+    }
+
+
+    function createMarker(plot,index,place_type) {
+                                    // If there are no photos exit function, marker not created
+           if (!plot.photos) {
+               return;
+           }
+
+           markers[index] = new google.maps.Marker({
+                    map: map,
+                    draggable: false,
+                    animation: google.maps.Animation.DROP,
+                    position: plot.geometry.location,
+                    title: plot.name,
+                    icon: Set_Icon (place_type) 
+                                    // Set custom ICON depending on the chosen activity
+            });
             
-            markers[index].placeResult=plot;
-            
+           markers[index].placeResult=plot;
+                                    // Plot contains the photo associated with the marker.
                  
            markers[index].addListener('click', function() {   
                 var prev_info = this;
@@ -94,18 +130,22 @@
                   placeId: this.placeResult.place_id,
                   fields: ['formatted_address', 'international_phone_number', 'reviews']
                 };
-               
+                                    // This search find the details of the marker.
+
               service = new google.maps.places.PlacesService(map);
 
               service.getDetails(request, function (place, status) {  
                  if (status == google.maps.places.PlacesServiceStatus.OK) {  
-                       
+                                    
+                                    // Marker info is sent to the DOM
                      placesList.innerHTML = "<p>"+ prev_info.placeResult.name + "<br> "
                                      + "Rating: " + valid_field(prev_info.placeResult.rating)+" stars"+"<br> "
                                      + "Address: " + place.formatted_address +"<br>"
                                      +"Phone: " + place.international_phone_number + "</p>"; 
                                
-                        
+                                     // Deletes existing Marker image on the DOM and replace
+                                     // it with the selected one.
+
                      if (img_parent.hasChildNodes()) {
                         $(img_parent).empty();
                      }
@@ -118,12 +158,13 @@
                      
                      myImage.appendTo($("#img-display"));
                    
-    
                  }
                });
                
                infowindow.setContent(prev_info.placeResult.name);
                infowindow.open(map, this);
+
+                                        // Animate the chosen marker only
 
                for (var i = 0; i < markers.length; i++) {  
                     markers[i].setAnimation(null)
@@ -134,58 +175,72 @@
 
     }
 
-            function Select_Activity () {
-                var criteria;
 
-               var activities = $("input[name~='type']");
-               
-               for(var i = 0; i < activities.length; i++){
-                  if(activities[i].checked){
-                     criteria = activities[i].value; 
-                    }
-                }  
-               return criteria;
-            }
+    function clearMarkers (MarkArray) {
+                                     
+        for (var i = 0; i < MarkArray.length; i++) {  
+             MarkArray[i].setMap(null);
+        }   
+
+         MarkArray = [];
+         MarkArray.length = 0;        
+    }
+
+
+    function Select_Activity () {
+    var criteria;
+                        // Select the name= 'type' fields within the input boxes
+                        // and return the value of the checked box
+
+    var activities = $("input[name~='type']");       
+        for(var i = 0; i < activities.length; i++){
+            if(activities[i].checked){
+                criteria = activities[i].value; 
+             }
+        }  
+        return criteria;
+    }
  
-            function toggleBounce(ele) {
-                 if (ele.getAnimation() !== null) {
-                   ele.setAnimation(null);
-                 } else {
-                  ele.setAnimation(google.maps.Animation.BOUNCE);
-                  }
-            }
+    function toggleBounce(ele) {
+        if (ele.getAnimation() !== null) {
+             ele.setAnimation(null);
+         } else {
+             ele.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
 
-            function valid_field (txt) {
-                     var tmp="";
-                     if (txt != null) {tmp = txt};
-                     return tmp;
-            }
+    function valid_field (txt) {
+          var tmp="";
+        if (txt != null) {tmp = txt};
+          return tmp;
+    }
 
-            function Set_Icon (Estab_type) {
-                var Icon_Str;
+                                    // Select ICON relating to the chosen activity.
+    function Set_Icon (Estab_type) {
+            var Icon_Str;
 
-                if (Estab_type == 'cafe') {
-                Icon_Str ='assets/images/icons8-coffee-to-go-48.png'}
-                else if (Estab_type == 'lodging') {
-                Icon_Str ='assets/images/icons8-hotel-40.png'}
-                else if (Estab_type == 'restaurant') {
-                Icon_Str ='assets/images/icons8-hamburger-50.png'}
-                else if (Estab_type == 'bar')  {
-                Icon_Str ='assets/images/icons8-bar-40.png'}
-                else if (Estab_type == 'museum') {
-                Icon_Str ='assets/images/icons8-museum-40.png'}
-                else {Icon_Str = null}
+         if (Estab_type == 'cafe') {
+         Icon_Str ='assets/images/icons8-coffee-to-go-48.png'}
+         else if (Estab_type == 'lodging') {
+         Icon_Str ='assets/images/icons8-hotel-40.png'}
+         else if (Estab_type == 'restaurant') {
+         Icon_Str ='assets/images/icons8-hamburger-50.png'}
+         else if (Estab_type == 'bar')  {
+         Icon_Str ='assets/images/icons8-bar-40.png'}
+         else if (Estab_type == 'museum') {
+         Icon_Str ='assets/images/icons8-museum-40.png'}
+         else {Icon_Str = null}
 
-                return Icon_Str;
-            }
+         return Icon_Str;
+    }
 
+                                    // Find the starting city
    
-       function Start_location (location, callback) {
+    function Start_location (location, callback) {
         var geocoder;
          
         geocoder = new google.maps.Geocoder();
         
-
         geocoder.geocode({'latLng': location}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             
@@ -204,6 +259,6 @@
         
         });
          
-       }
+    }
     
             
